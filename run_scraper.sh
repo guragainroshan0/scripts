6a45e6380027fc3de31449be7c0cf393631dfce3
Roshan Guragain <guragainroshan0@gmail.com>
2021-05-22 15:04:35 +0545

    COMMENTS

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# needs python3, pip3 and firefox packages
+# not tested yet 
+# will test if i have to do the similar work again
+# needs python3, pip3 and firefox packages, pip3 requires python3-pip to install so didnot add in the array
 packages=(python3 firefox screen)
 
 # loop through the packages to install
 for pkg in $packages
 do
+	# check if packages exists
 	which $pkg > /dev/null 2>&1
 
-	if [ $? != 0]
+	# if the package exits exit status ($?) is set to 0  and if not 1
+	if [ $? != 0 ]
 	then
 		sudo apt install $pkg
 	fi
@@ -24,8 +28,10 @@ fi
 
 # install the requirements
 pip3 install -r requirements.txt
+
 # copy the geckodriver binary to the path
 cp geckodriver /usr/bin/geckodriver
 
+# run the scraper in screen sessions
 screen -S RUN -m -d python3 run.py
 screen -S SEND -m -d python3 send.py
