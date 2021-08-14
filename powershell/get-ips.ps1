d6fabd5c1d7d7a43fc16465b40f804f9c9dc0931
Roshan Guragain <guragainroshan0@gmail.com>
2021-08-14 14:15:14 +0545

    all ips done

--- powershell/get-ips.ps1
+++ powershell/get-ips.ps1
@@ -4,9 +4,32 @@ Get ips using ipconfig and select-string to filter just the ips.
 $ips is an array so loop through it to get the ip's associated.
 
 #>
-$ips=(ipconfig | select-string IPv4)
-for($i=0;$i -lt $ips.Count;$i++)
-{
-	echo $ips[$i].ToString().Split(':')[1];
-
+function Get-IP($ips) {
+	$address=@();
+	for ($i = 0; $i -lt $ips.Count; $i++) {
+		$address =$address + $ips[$i].ToString().Split('')[-1];
+	}
+	return $address;
 }
+
+Write-Output "Local IPv4 Address : `n"
+$ipv4 = (ipconfig | select-string IPv4)
+Get-IP($ipv4);
+
+Write-Output "`n`nPublic IPv4 Address : `n"
+Write-Output (Invoke-WebRequest "https://ifconfig.me" ).content
+
+Write-Output "`n`nLocal IPv6 Address : `n"
+$ipv6 = (ipconfig | select-string IPv6)
+Get-IP($ipv6);
+
+Write-Output "`n`nPublic IPv6 Address : `n"
+Write-Output (Invoke-WebRequest "https://api64.ipify.org").content
+
+
+
+
+
+
+
+
